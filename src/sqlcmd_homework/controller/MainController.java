1df1d580afce49c38d5ac6ef3276e27a3a359448
Sims <remable@gmail.com>
2015-10-06 22:36:45 +0200

    Реализована работа с командами 'list' - to show all existing table names 'find|tableName' - to show table 'tableName' content 'help' - to show help for this project 'exit' - to exit the program

--- src/sqlcmd_homework/controller/MainController.java
+++ src/sqlcmd_homework/controller/MainController.java
@@ -1,5 +1,6 @@
 package sqlcmd_homework.controller;
 
+import sqlcmd_homework.model.DataSet;
 import sqlcmd_homework.model.DatabaseManager;
 import sqlcmd_homework.view.View;
 
@@ -25,23 +26,63 @@ public class MainController {
             String command = view.read();
 
             if (command.equals("list")) {
-                doList();
+                showList();
             } else if (command.equals("help")) {
-                doHelp();
+                showHelp();
+            } else if (command.equals("exit")) {
+                view.write("Bye");
+                System.exit(0);
+            } else if (command.startsWith("find")) {
+                findTable(command);
             } else {
                 view.write("Non existing command: " + command);
             }
         }
-
     }
 
-    private void doHelp() {
+    private void findTable(String command) {
+        String[] data = command.split("\\|");
+        String tableName = data[1];
+        String[] tableColumns = manager.getTableColumns(tableName);
+        printHeader(tableColumns);
+        DataSet[] tableData = manager.getTableData(tableName);
+        printTable(tableData);
+    }
+
+    private void printTable(DataSet[] tableData) {
+        for (DataSet row: tableData){
+            printRow(row);
+        }
+    }
+
+    private void printRow(DataSet row) {
+        Object[] values = row.getValues();
+        String result = "";
+        for (Object value: values){
+            result += value + "|";
+        }
+        view.write(result);
+    }
+
+    private void printHeader(String[] tableColumns) {
+        String result = "";
+        for (String name: tableColumns){
+            result += name + "|";
+        }
+        view.write("-----------------");
+        view.write(result);
+        view.write("-----------------");
+    }
+
+    private void showHelp() {
         view.write("Existing commands:");
         view.write("\t 'list' - to show all existing table names");
+        view.write("\t 'find|tableName' - to show table 'tableName' content");
         view.write("\t 'help' - to show help for this project");
+        view.write("\t 'exit' - to exit the program");
     }
 
-    private void doList() {
+    private void showList() {
         String[] tableNames = manager.getTableNames();
         String message = Arrays.toString(tableNames);
         view.write(message);
