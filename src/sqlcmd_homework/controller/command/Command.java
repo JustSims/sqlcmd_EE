57103f2a7bbad1e520f64600873f4f5a0f01b49b
Sims <remable@gmail.com>
2015-10-14 22:24:57 +0200

    Перевожу проект на Maven

--- src/sqlcmd_homework/controller/command/Command.java
+++
@@ -1,9 +0,0 @@
-package sqlcmd_homework.controller.command;
-
-/**
- * Created by Sims on 12/10/2015.
- */
-public interface Command {
-    boolean canProcess(String command);
-    void process(String command);
-}
