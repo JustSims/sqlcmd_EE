35481d1f93d52737658130f9bb9ed34097e71741
Sims <remable@gmail.com>
2015-10-28 22:11:52 +0100

    добавлен юнит-тест для комманды Help

---
+++ src/test/java/ua/com/juja/sqlcmd_homework/controller/command/HelpTestWithMockito.java
@@ -0,0 +1,44 @@
+package ua.com.juja.sqlcmd_homework.controller.command;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import ua.com.juja.sqlcmd_homework.model.DataSet;
+import ua.com.juja.sqlcmd_homework.model.DatabaseManager;
+import ua.com.juja.sqlcmd_homework.view.View;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Sims on 27/10/2015.
+ */
+public class HelpTestWithMockito {
+
+    private DatabaseManager manager;
+    private View view;
+    private Command command;
+
+    @Before
+    public void setUp(){
+        manager = mock(DatabaseManager.class);
+        view = mock(View.class);
+        command = new Help(view);
+    }
+
+
+    @Test
+    public void testCanProcessHelp() {
+        //given
+
+        //when
+        boolean canProcess = command.canProcess("help");
+
+        //then
+        assertTrue(canProcess);
+    }
+
+}
