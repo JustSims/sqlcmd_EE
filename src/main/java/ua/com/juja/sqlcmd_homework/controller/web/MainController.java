ee7839c0ea4d9d472cfe5ff8c569205e89238cd0
Sims <remable@gmail.com>
2015-12-21 21:49:42 +0100

    added menu to MainController

--- src/main/java/ua/com/juja/sqlcmd_homework/controller/web/MainController.java
+++ src/main/java/ua/com/juja/sqlcmd_homework/controller/web/MainController.java
@@ -1,9 +1,13 @@
 package ua.com.juja.sqlcmd_homework.controller.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.juja.sqlcmd_homework.service.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Sims on 17/12/2015.
@@ -11,8 +15,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private Service service;
+
     @RequestMapping(value = "/help", method = RequestMethod.GET)
     public String help() {
         return "help";
     }
+
+    @RequestMapping(value = "/menu", method = RequestMethod.GET)
+    public String menu(HttpServletRequest request) {
+        request.setAttribute("items", service.commandsList());
+        return "menu";
+    }
+
+
+
+
+
+
 }
